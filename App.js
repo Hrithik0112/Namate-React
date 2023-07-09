36d37e75f55fa44def9898dc7b274ce866c4a385
Hrithik <slumdog4321@gmail.com>
2023-07-09 16:28:58 +0530

    Chapter 2-Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+
 const heading = React.createElement("h1", {id: "title",}, "hello dosto");
 const head2 = React.createElement("h2", {id: "title",}, "hello dosto");
 const container = React.createElement("div", {id: "container",}, [heading , head2]);
