9d9aff12caf649d82eac735b6ead50108eac28f7
Hrithik <slumdog4321@gmail.com>
2023-07-09 16:56:26 +0530

    Chapter3- Laying the Founation

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
+import { directive } from "@babel/types";
 import React from "react";
 import ReactDOM from "react-dom/client"
 
 
 
-const heading = React.createElement("h1", {id: "title",}, "hello dosto");
-const head2 = React.createElement("h2", {id: "title",}, "hello dosto");
-const container = React.createElement("div", {id: "container",}, [heading , head2]);
+// const heading = React.createElement("h1", {id: "title",}, "hello dosto");
+// const head2 = React.createElement("h2", {id: "title",}, "hello dosto");
+// const container = React.createElement("div", {id: "container",}, [heading , head2]);
             
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+const Title = () => (
+    <h1 id="title" key="h2">
+        namaste raect
+    </h1>
+);
+
+const HeadComponent = () => {
+    return (
+        <div>
+            <Title/>
+            <h2>this is raect functional component</h2>
+            <h2>this is h2 tag</h2>
+        </div>
+    )
+}
+    
+    
+    
+    
+    
+    
+const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+    
+root.render(<HeadComponent/>);
