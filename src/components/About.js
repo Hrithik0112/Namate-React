96b981aeb8821971683a89caace1518e76431acc
Hrithik <slumdog4321@gmail.com>
2023-07-11 09:30:30 +0530

    Chapter 08- Lets's Get Classy (Class Components)

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,40 @@
-const About = () => {
-    return(
-        <div>
-            <h1>This Is About Us Page</h1>
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>This Is About Us Page</h1>
 
-            <p> this ia chapter 7 of namaste react </p>
-        </div>
-    )
+//             <p> this ia chapter 7 of namaste react </p>
+//         </div>
+//     )
+// }
+
+// export default About;
+
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    //console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    //console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    //console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass name={"First"} location={"Dehradun Class"} />
+      </div>
+    );
+  }
 }
 
 export default About;
